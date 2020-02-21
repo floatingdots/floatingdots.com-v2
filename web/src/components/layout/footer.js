887d6dd6256a6344fb004704f7040b5e9ff25937
Naoto Yoshimoto <naotone@floatingdots.com>
2020-02-21 16:59:04 -0500

    Add footer

--- web/src/components/layout/footer.js
+++ web/src/components/layout/footer.js
@@ -9,92 +9,83 @@ const Wrapper = styled.footer`
   position: relative;
   z-index: 50;
   background: ${colors.black};
-  color: ${colors.white};
-  padding: 2.4rem 1.6rem 4rem 1.6rem;
-  @media (min-width: 1024px) {
-    padding-top: 8rem;
-    margin-top: 8rem;
-  }
+  color: #eee;
 `
 
 const Inner = styled.div`
-  display: block;
   max-width: 1024px;
   margin: 0 auto;
-`
-
-const Title = styled.span`
-  font-family: ${fontfamily.handwriting};
-  display: block;
-  font-size: 1.8rem;
-`
-const SubTitle = styled.span`
-  font-family: ${fontfamily.handwriting};
-  display: block;
-  font-size: 1.0rem;
-`
-
-const Header = styled.div`
-  display: block;
-  margin: 0 0 2.4rem 0;
+  padding: 4rem 16px 2rem 16px;
+  @media (min-width: 1024px) {
+    padding: 8rem 32px 4rem 32px;
+    margin-top: 8rem;
+  }
 `
 
 const Nav = styled.nav`
   display: block;
 `
 
+const Groups = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+
+`
+
+const Group1 = styled.div`
+  flex: 0 0 70%;
+  margin: 0 0 4rem 0;
+  @media (min-width: 768px) {
+    margin: 0;
+  }
+`
+const Group2 = styled.div`
+  flex: 0 0 30%;
+  margin: 0 0 4rem 0;
+  @media (min-width: 768px) {
+    margin: 0;
+  }
+`
+
 const List = styled.ul`
   display: block;
-  font-family: ${fontfamily.handwriting};
-  font-size: ${props => !props.small ? '3.4rem' : '1.4rem'};
-  /* font-weight: ${props => !props.small ? '700' : '400'}; */
+  font-size: ${props => !props.small ? '3.2rem' : '1.4rem'};
   margin-bottom:  ${props => !props.small ? '4.0rem' : 'none'};
+  font-weight: 700;
 `
 
 const Item = styled.li`
-  margin: 0 0 2.0rem 0;
+  margin: 0 0 3.6rem 0;
 `
 
 const StyledLink = styled(props => <Link {...props} />)`
-  color: white;
+  color: #eee;
   text-decoration: none;
+`
+
+const Email = styled.a`
+  font-size: 2rem;
+  font-weight: 700;
+  color: #eee;
+  display: inline-block;
+  margin: 0 0 4rem 0;
+`
+
+const Address = styled.span`
+  display: block;
+  font-size: 2rem;
+  font-weight: 700;
 `
 
 const Copyright = styled.span`
-  opacity: 0.7;
+  opacity: 0.5;
   font-size: 1.2rem;
   padding-top: 2.4rem;
   display: block;
-`
-
-const StyledExternal = styled.a`
-  color: ${colors.white};
-  text-decoration: none;
-`
-
-const Socials = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin: 0 0 1.8rem 0;
-`
-
-const Social = styled.a`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0 4rem 0 0;
-`
-
-const Icon = styled.img`
-  display: block;
-  width: 32px;
-`
-
-const Small = styled.span`
-  font-size: 1.2rem;
-  margin: 0 0 0 2.4rem;
-  opacity: 0.8;
-  font-family: ${fontfamily.ja};
+  letter-spacing: 0.02rem;
 `
 
 class Footer extends React.Component {
@@ -102,11 +93,22 @@ class Footer extends React.Component {
     return (
       <Wrapper>
         <Inner>
-          <Nav>
-            <List>
-              {/* <Item><StyledLink to='/news/'>News</StyledLink></Item> */}
-            </List>
-          </Nav>
+          <Groups>
+            <Group1>
+              <Nav>
+                <List>
+                  <Item><StyledLink to='/projects/'>Projects</StyledLink></Item>
+                  <Item><StyledLink to='/blog/'>Blog</StyledLink></Item>
+                  <Item><StyledLink to='/about/'>About</StyledLink></Item>
+                  <Item><StyledLink to='/careers/'>Careers</StyledLink></Item>
+                </List>
+              </Nav>
+            </Group1>
+            <Group2>
+              <Email href='mail:hi@floatingdots.com'>hi@floatingdots.com</Email>
+              <Address>530 Fifth Ave,<br />Floor9 #13,<br />New York, NY 10036</Address>
+            </Group2>
+          </Groups>
           <Copyright>&copy; Floating Dots, LLC</Copyright>
         </Inner>
       </Wrapper>

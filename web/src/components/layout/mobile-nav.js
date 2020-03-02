af24c59e0701e2327518fa2e1a25e0b38e1493b3
Naoto Yoshimoto <naotone@floatingdots.com>
2020-03-02 17:09:45 -0500

    Fix mobile nav toggle

--- web/src/components/layout/mobile-nav.js
+++ web/src/components/layout/mobile-nav.js
@@ -61,11 +61,11 @@ const MobileNav = ({onHideNav, onShowNav, showNav}) => {
     <Wrapper className={(showNav && 'active')}>
       <Inner>
         <List>
-          <Item><StyledLink to={i18n.language === 'en' ? '/projects/' : `/${i18n.language}/projects/`}>Projects</StyledLink></Item>
-          <Item><StyledLink to={i18n.language === 'en' ? '/blog/' : `/${i18n.language}/blog/`}>Blog</StyledLink></Item>
-          <Item><StyledLink to={i18n.language === 'en' ? '/about/' : `/${i18n.language}/about/`}>About</StyledLink></Item>
-          <Item><StyledLink to={i18n.language === 'en' ? '/contact/' : `/${i18n.language}/contact/`}>Contact</StyledLink></Item>
-          <Item><StyledLink to={i18n.language === 'en' ? '/careers/' : `/${i18n.language}/careers/`}>Careers</StyledLink></Item>
+          <Item><StyledLink onClick={onHideNav} to={i18n.language === 'en' ? '/projects/' : `/${i18n.language}/projects/`}>Projects</StyledLink></Item>
+          <Item><StyledLink onClick={onHideNav} to={i18n.language === 'en' ? '/blog/' : `/${i18n.language}/blog/`}>Blog</StyledLink></Item>
+          <Item><StyledLink onClick={onHideNav} to={i18n.language === 'en' ? '/about/' : `/${i18n.language}/about/`}>About</StyledLink></Item>
+          <Item><StyledLink onClick={onHideNav} to={i18n.language === 'en' ? '/contact/' : `/${i18n.language}/contact/`}>Contact</StyledLink></Item>
+          <Item><StyledLink onClick={onHideNav} to={i18n.language === 'en' ? '/careers/' : `/${i18n.language}/careers/`}>Careers</StyledLink></Item>
         </List>
         <LanguageSwitcher />
       </Inner>

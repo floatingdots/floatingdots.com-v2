4fa893b4ee2d536250f71c0cf6ad77a82d62d982
Naoto Yoshimoto <naotone@floatingdots.com>
2020-03-11 16:34:50 -0400

    Update shcema validations

--- studio/schemas/lib/helper.js
+++ studio/schemas/lib/helper.js
@@ -1,8 +1,19 @@
-export function ValidationLocaleRequired (el, name) {
-  if (!el['en'] || el['en'] === undefined) {
-    return `${name} - En is Required.`
-  }
-  if (!el['ja'] || el['ja'] === undefined) {
-    return `${name} - Ja is Required.`
+const locales = ['en', 'ja']
+
+export function ValidationLocaleRequired (el, context, name) {
+  let message = ''
+  locales.map(locale => {
+    if (context.document.title && context.document.title[locale]) {
+      if (!el || !el[locale]) {
+        const l = locale.charAt(0).toUpperCase() + locale.slice(1)
+        message += `${name}(${l}) is Required.\n`
+      }
+    }
+  })
+  return message
+}
+export function ValidationLocaleTitleRequired (el, context, name) {
+  if (locales.every(locale => !el[locale])) {
+    return 'Title is Required.'
   }
 }
